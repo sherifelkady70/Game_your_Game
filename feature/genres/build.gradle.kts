57488186743fd49c822dd0d57f8455c7a3f83705
sherif Abdulrahman <sherifabdulrahman44@gmail.com>
2026-02-11 02:38:39 +0200

    setup test dependencies.

--- feature/genres/build.gradle.kts
+++ feature/genres/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
